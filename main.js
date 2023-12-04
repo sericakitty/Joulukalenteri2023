56d1d42f1f0de11db14363d5b116bb88a72df4d9
sericakitty <63978546+sericakitty@users.noreply.github.com>
2023-12-04 16:44:01 +0200

    tähtien animoiminen lisätty

--- main.js
+++ main.js
@@ -16,11 +16,32 @@ const createCalendar = () => {
     calendarDoorText.innerHTML = i + 1;
     calendarDoor.appendChild(calendarDoorText);
 
-    courseNumber = i + 1;
+    courseNumber = i;
     let coursePath = '';//kuvien lisäys tähän, videolla 32.32
-    
-    calendarDoorText.addEventListener("click", openDoor); //jäi kohtaan 34:28
+
+    // calendarDoorText.addEventListener("click", openDoor); //jäi kohtaan 34:28
 
 }
+
 }
 calendarButton.addEventListener("click", createCalendar);
+
+// stars animation
+document.addEventListener('DOMContentLoaded', function() {
+  const starContainer = document.querySelector('.star-container');
+  const numStars = 100; // Adjust the number of stars here
+
+  for (let i = 0; i < numStars; i++) {
+    let star = document.createElement('div');
+    star.className = 'star';
+    star.style.top = `${Math.random() * 100}%`;
+    star.style.left = `${Math.random() * 100}%`;
+    star.style.animationDelay = `${Math.random() * 2}s`;
+
+    if (Math.random() > 0.5) {
+      star.classList.add('twinkle');
+    }
+
+    starContainer.appendChild(star);
+  }
+});
